fc9bcfc29d40793c74a9ffe01ddb0616dd58ba86
FourSqweez <four7518@gmail.com>
2022-08-23 18:22:02 +0700

    Update : sent data with props to cardItem

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
+import CardItem from './CardItem'
 
-export default function Cards() {
-	return <div>MainPage</div>
+export default function Cards({ characters }) {
+	return (
+		<div className='flex w-full my-2 justify-center'>
+			<div className="grid grid-cols-4 gap-6 text-center">
+			{characters &&
+				characters.map((item) => <CardItem key={item.id} item={item} />)}
+		</div>
+		</div>
+	)
 }
