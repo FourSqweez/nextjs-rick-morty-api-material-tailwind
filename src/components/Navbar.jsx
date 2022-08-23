82c080cda25d8e7400a62c543fb9ceb390b056c2
FourSqweez <four7518@gmail.com>
2022-08-23 18:32:45 +0700

    Add : Created blank components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='flex '>
+      <div className='relative mb-10'>Navbar Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure ipsum mollitia assumenda beatae. Nesciunt eligendi accusamus vel officia architecto obcaecati? Quam dolorem doloremque asperiores iste explicabo quidem? Debitis, sit consequatur.</div>
+    </div>
+  )
+}
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SideBar() {
+  return (
+    <div className='max-w-sm w-full'>SideBar</div>
+  )
+}
