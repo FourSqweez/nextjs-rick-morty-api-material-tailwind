e4276150828a0fcf0d87329ca87866ef6ba3502e
FourSqweez <four7518@gmail.com>
2022-08-23 15:06:29 +0700

    Update : Material tailwind config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { Button } from '@material-tailwind/react'
+
 
 export default function Home() {
 	return (
@@ -12,6 +14,7 @@ export default function Home() {
 
 			<main>
 				<h1 className="text-red-500 text-2xl">Test</h1>
+				<Button>Button</Button>
 			</main>
 		</div>
 	)
