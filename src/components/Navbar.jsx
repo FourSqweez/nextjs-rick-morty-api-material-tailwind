d8ab82b379e8e6601a06b6aa6ffea788375e332b
FourSqweez <four7518@gmail.com>
2022-09-12 19:43:43 +0700

    Add : Created Search component and used it in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
+import Search from './Search'
 
 export default function Navbar() {
-  return (
+	return (
 		<div className="flex fixed z-50 w-full h-16 bg-brown-500 items-center">
 			<div className="flex w-40 ml-4">Rick and Morty</div>
 			<div className="w-full flex justify-center">
-				<input
-					type="text"
-					className="w-full h-10 max-w-xl border-2 border-black rounded-md mr-10"
-				/>
+				<Search />
 			</div>
 		</div>
-  )
+	)
 }
---
+++ src/components/Search.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Search = () => {
+	return (
+		<>
+			<input
+				type="text"
+				className="w-full h-10 max-w-xl border-2 border-black rounded-md mr-10"
+			/>
+		</>
+	)
+}
+
+export default Search
