863e1448e08a2cb432fc8ebefbe2bdaaa4efc34a
FourSqweez <four7518@gmail.com>
2022-09-12 13:49:33 +0700

    Add : Add context provider and created filter state

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,16 @@
 import '../styles/globals.css'
 import { ThemeProvider } from '@material-tailwind/react'
+import { AppContext } from '../app/contextApi'
+import { useState } from 'react'
 
 function MyApp({ Component, pageProps }) {
+	const [filters, setFilters] = useState(null)
 	return (
-		<ThemeProvider>
-			<Component {...pageProps} />
-		</ThemeProvider>
+		<AppContext.Provider value={{ filters, setFilters }}>
+			<ThemeProvider>
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</AppContext.Provider>
 	)
 }
 
