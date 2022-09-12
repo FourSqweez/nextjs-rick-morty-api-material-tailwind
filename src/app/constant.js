5701147eab2f6603697722b30c9e69dab52bb98c
FourSqweez <four7518@gmail.com>
2022-09-12 14:20:31 +0700

    Add : Created constant variable

---
+++ src/app/constant.js
@@ -0,0 +1,13 @@
+export const STATUS = 'Status'
+export const GENDER = 'Gender'
+export const SPECIES = 'Species'
+
+export const MALE = 'Male'
+export const FEMALE = 'Female'
+export const HUMAN = 'Human'
+export const ALIEN = 'Alien'
+
+export const DEAD = 'Dead'
+export const ALIVE = 'Alive'
+export const UNKNOWN = 'unknown'
+export const NULL = null
