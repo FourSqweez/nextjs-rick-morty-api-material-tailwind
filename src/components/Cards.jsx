427976af4a66fd045b35603245e71b3e0b939aab
FourSqweez <four7518@gmail.com>
2022-09-12 14:21:23 +0700

    Add : Filter condition to filter from characters

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,35 +1,66 @@
 import React, { useContext, useEffect, useState } from 'react'
 import CardItem from './CardItem'
 import { AppContext } from '../app/contextApi'
+import {
+	ALIEN,
+	ALIVE,
+	DEAD,
+	FEMALE,
+	HUMAN,
+	MALE,
+	NULL,
+	UNKNOWN,
+} from '../app/constant'
 
 export default function Cards({ characters }) {
 	const [displays, setDisplays] = useState(characters)
 	const { filters } = useContext(AppContext)
 
+	console.log('Character : ', displays)
+
 	console.log('Test from Cards : ', filters)
 
 	useEffect(() => {
-		if (filters !== null) {
+		if (filters !== NULL) {
 			handleDisplays(filters)
 		} else {
 			setDisplays(characters)
 		}
 	}, [filters])
 
-	const setFilter = () => {
-		const newDisplays = characters.map((item) => item.status == '')
+	const getFilterFromStatus = (status) => {
+		return characters.filter((item) => item.status == status)
+	}
+	const getFilterFromGender = (gender) => {
+		return characters.filter((item) => item.gender == gender)
+	}
+
+	const getFilterFromSpecies = (species) => {
+		return characters.filter((item) => item.species == species)
 	}
 
 	const handleDisplays = (filters) => {
 		switch (filters) {
-			case 'Dead':
-				setDisplays()
+			case DEAD:
+				setDisplays(getFilterFromStatus(filters))
 				return
-			case 'unknown':
-				setDisplays()
+			case UNKNOWN:
+				setDisplays(getFilterFromStatus(filters))
 				return
-			case 'Alive':
-				setDisplays()
+			case ALIVE:
+				setDisplays(getFilterFromStatus(filters))
+				return
+			case MALE:
+				setDisplays(getFilterFromGender(filters))
+				return
+			case FEMALE:
+				setDisplays(getFilterFromGender(filters))
+				return
+			case HUMAN:
+				setDisplays(getFilterFromSpecies(filters))
+				return
+			case ALIEN:
+				setDisplays(getFilterFromSpecies(filters))
 				return
 		}
 	}
