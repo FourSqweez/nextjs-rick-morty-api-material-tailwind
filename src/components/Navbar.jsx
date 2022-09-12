51ab713529271d438581cf99ac188a7d5419818c
FourSqweez <four7518@gmail.com>
2022-09-12 17:57:01 +0700

    Update : Styled Navbar to fixed and input

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div className='flex '>
-      <div className='relative mb-10'>Navbar Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure ipsum mollitia assumenda beatae. Nesciunt eligendi accusamus vel officia architecto obcaecati? Quam dolorem doloremque asperiores iste explicabo quidem? Debitis, sit consequatur.</div>
-    </div>
+		<div className="flex fixed z-50 w-full h-16 bg-brown-500 items-center">
+			<div className="flex w-40 ml-4">Rick and Morty</div>
+			<div className="w-full flex justify-center">
+				<input
+					type="text"
+					className="w-full h-10 max-w-xl border-2 border-black rounded-md mr-10"
+				/>
+			</div>
+		</div>
   )
 }
